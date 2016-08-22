8f4e68e904c600fdb6c01bcb3135f47c8dbe6dbb
JanChwiejczak <jchwiejczak@gmail.com>
2016-08-22 15:32:08 +0100

    Fix indent error, change spaces to '_'

--- 02_chess_board.py
+++ 02_chess_board.py
@@ -2,26 +2,26 @@
 size = 4
 
 # First Method
-even_row = '# ' * (size // 2)
-odd_row = ' #' * (size // 2)
+even_row = '#_' * (size // 2)
+odd_row = '_#' * (size // 2)
 if size % 2 != 0:
     even_row += '#'
-    odd_row += ' '
+    odd_row += '_'
 board = [even_row if x % 2 == 0 else odd_row for x in range(1, size + 1)]
 print('\n'.join(board))
 
 
 # Second Method - Using a simple piece generator
 def board_piece():
-    pieces = ' #'
+    pieces = '_#'
     while True:
         for p in pieces:
             yield p
 piece = board_piece()
 
 for i in range(1, (size ** 2) + 1):
-    if i % size:
+    if i % size != 0:
         print(next(piece), end='')
-     else:
+    else:
         print(next(piece)) # Last piece in a row printed with newline
         next(piece)  # Switch first piece in next row
