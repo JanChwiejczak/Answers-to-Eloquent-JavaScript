dbb1b175ad08806a4b349e798089db30bf645252
Jan Chwiejczak <jchwiejczak@gmail.com>
2016-09-11 11:42:21 +0100

    Define RangeSeq object both with inheritence and without

--- 06_sequence_interface.js
+++ 06_sequence_interface.js
@@ -11,13 +11,44 @@ Object.defineProperty(SeqArray.prototype, "exhausted", {
 
 SeqArray.prototype.next = function() {
     if (!this.exhausted) {
-        var next =  this.array[this.state];
-        this.state += 1;
-        return next
+        this.state++;
+        return this.array[this.state - 1];
     }
 };
 
 
+// Inheriting from SeqArray prototype
+function RangeSeq(from, to) {
+    this.state = 0;
+    this.array = new Array(to - from + 1).fill();
+    for (var i=0; i < this.array.length; i++) {
+        this.array[i] = from + i;
+    }
+}
+
+RangeSeq.prototype = Object.create(SeqArray.prototype);
+
+
+// No Inheritance
+function RangeSeq(from, to) {
+    this.state = from;
+    this.to = to;
+}
+
+Object.defineProperty(RangeSeq.prototype, "exhausted", {
+    get: function() {
+        return this.state > this.to
+    }
+});
+
+RangeSeq.prototype.next = function() {
+    if (!this.exhausted) {
+        this.state++;
+        return this.state - 1
+    }
+}
+
+// Helper function
 var LogFive = function(sequence) {
    for (var i = 0; i < 5; i++) {
        if (sequence.exhausted) { break; }
@@ -26,3 +57,4 @@ var LogFive = function(sequence) {
 };
 
 LogFive(new SeqArray([1, 2, 3 ]));
+LogFive(new RangeSeq(103, 1005));
